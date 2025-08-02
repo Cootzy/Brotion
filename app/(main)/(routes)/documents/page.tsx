9a4c6b79db4065d81bc242c78eafea4d464ca509
Cootzy <umastryxsubterror@gmail.com>
2025-08-03 06:25:39 +0700

    Implement User Settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
-  return <div className="font bold text-2xl">This is a protected page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/test.jpg"
+        height="300"
+        width="300"
+        alt="test"
+        className="dark:hidden"
+      />
+      <Image
+        src="/test1.jpg"
+        height="300"
+        width="300"
+        alt="test"
+        className="hidden dark:block"
+      />
+
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Brotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create Something
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
