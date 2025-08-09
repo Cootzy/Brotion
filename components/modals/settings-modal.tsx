921c5f7cd0a6aae48268e2751cf13d270e741d4b
Cootzy <umastryxsubterror@gmail.com>
2025-08-09 21:04:18 +0700

    Implement Advanced File Upload (Delete, Replace)

--- components/modals/settings-modal.tsx
+++ components/modals/settings-modal.tsx
@@ -18,7 +18,8 @@ export const SettingsModal = () => {
           <div className="flex flex-col gap-y-1">
             <Label>Appearance</Label>
             <span className="text-[0.8rem] text-muted-foreground">
-              Customize how Brotion looks on your device.
+              Customize how Brotion looks on your device. Light is better trust
+              me
             </span>
           </div>
           <ModeToggle />
